7f4396b487d4ca2f4d04b3b7bc3a24a70bd5e27b
Jesus Oseguera <osegueraj286@gmail.com>
2024-03-18 15:56:33 -0700

    Create card class within card.cpp

---
+++ card.cpp
@@ -0,0 +1,15 @@
+class card
+{
+private:
+  std::string name;
+  int mana;
+  int attack;
+  int health;
+public:
+  std::string getName();
+  int getMana();
+  int getAttack();
+  int getHealth();
+}
+
+#endif
