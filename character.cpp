799e12638ffff3e5b1820c33977309f0a6662754
Jesus Oseguera <osegueraj286@gmail.com>
2024-03-25 14:21:34 -0700

    Create character.cpp
    Began writing function implementations

---
+++ character.cpp
@@ -0,0 +1,13 @@
+character::character()
+{
+  // Base constructor for the character class
+  health = 0;
+  armor = 0;
+  speed = 0
+}
+
+int character::character getHealth()
+{
+  // Returns the health from the base class
+  return health;
+}
